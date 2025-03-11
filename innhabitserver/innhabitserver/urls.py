ca3adbf54f6c3c8fb67b9d3e79caccd193cb63c8
DNIIBOY <daniel.nettelfield@gmail.com>
2025-03-11 17:31:03 +0100

    Add browser reload

--- innhabitserver/innhabitserver/urls.py
+++ innhabitserver/innhabitserver/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("__reload__/", include("django_browser_reload.urls")),
     path("", include("occupancy.urls")),
     path("admin/", admin.site.urls),
     path("api/v1/", include("api.urls")),
