adec5fe6eba28e373b80deb411801afa20a02ebb
DNIIBOY <daniel.nettelfield@gmail.com>
2025-03-12 21:10:58 +0100

    Use htmx for swapping insights

--- innhabitserver/dashboard/components/current_occupancy/current_occupancy.py
+++ innhabitserver/dashboard/components/current_occupancy/current_occupancy.py
@@ -1,13 +1,13 @@
+from django.utils import timezone
 from django_components import Component, register
 from occupancy.models import EntryEvent, ExitEvent
-from django.utils import timezone
 
 
 @register("current_occupancy")
 class CurrentOccupancy(Component):
     template_name = "current_occupancy.html"
 
-    def get_context_data(self) -> None:
+    def get_context_data(self) -> dict:
         midnight = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
         entries = EntryEvent.objects.filter(timestamp__gte=midnight).count()
         exits = ExitEvent.objects.filter(timestamp__gte=midnight).count()
--- innhabitserver/dashboard/views.py
+++ innhabitserver/dashboard/views.py
@@ -1,7 +1,9 @@
 import random
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+
 from django.core.exceptions import ValidationError
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django_components import DynamicComponent
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -15,11 +17,19 @@ def insights(request: HttpRequest) -> HttpResponse:
         "current_occupancy",
         "cafeteria_day",
     ]
+    locked_insight = False
     insight = request.GET.get("insight", None)
     if insight:
         if insight not in components:
             raise ValidationError("Invalid insight")
+        locked_insight = True
     else:
         insight = random.choice(components)
-    context = {"insight_name": insight}
+
+    if request.htmx:
+        return DynamicComponent.render_to_response(
+            kwargs={"is": insight},
+            request=request,
+        )
+    context = {"insight_name": insight, "locked_insight": locked_insight}
     return render(request, "insights.html", context=context)
