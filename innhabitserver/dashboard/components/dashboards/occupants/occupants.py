e642d7b7ced2a112ba1bfdbfdfcfee745ac2e52a
Boltz333 <boltz333yt@gmail.com>
2025-03-14 11:02:55 +0100

    1styk af dashboard

--- innhabitserver/dashboard/components/dashboards/occupants/occupants.py
+++ innhabitserver/dashboard/components/dashboards/occupants/occupants.py
@@ -1,6 +1,14 @@
+from django.utils import timezone
 from django_components import Component, register
+from occupancy.models import EntryEvent, ExitEvent
 
 
 @register("occupants")
 class Occupants(Component):
     template_name = "occupants.html"
+
+    def get_context_data(self) -> dict:
+        today = timezone.now().date()
+        entries = EntryEvent.objects.filter(timestamp__date=today).count()
+        exits = ExitEvent.objects.filter(timestamp__date=today).count()
+        return {"humans": entries - exits}
