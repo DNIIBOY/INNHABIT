82c883abadcc95f6a6ab9148fed5fb452ac3a414
DNIIBOY <daniel.nettelfield@gmail.com>
2025-03-16 23:31:35 +0100

    Add Labbeled Dates + create busiest_days component

--- innhabitserver/dashboard/components/dashboards/latest_events/latest_events.py
+++ innhabitserver/dashboard/components/dashboards/latest_events/latest_events.py
@@ -7,14 +7,14 @@ from occupancy.models import EntryEvent, ExitEvent
 class LatestEvents(Component):
     template_name = "latest_events.html"
 
-    def get_context_data(self) -> dict:
+    def get_context_data(self, items: int = 4) -> dict:
         entry_events = EntryEvent.objects.annotate(
             is_entry=Value(True, output_field=BooleanField())
         ).prefetch_related("entrance")
         exit_events = ExitEvent.objects.annotate(
             is_entry=Value(False, output_field=BooleanField())
         ).prefetch_related("entrance")
-        latest_events = entry_events.union(exit_events).order_by("-timestamp")[:4]
+        latest_events = entry_events.union(exit_events).order_by("-timestamp")[:items]
         return {
             "events": latest_events,
         }
