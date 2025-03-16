0c891a8d257828504a30d95b12600b001407b85e
DNIIBOY <daniel.nettelfield@gmail.com>
2025-03-16 23:31:35 +0100

    Add Labbeled Dates + create busiest_days component

--- innhabitserver/dashboard/components/dashboards/busiest_days/busiest_days.py
+++ innhabitserver/dashboard/components/dashboards/busiest_days/busiest_days.py
@@ -1,6 +1,35 @@
+from dashboard.models import LabelledDate
+from django.db.models import Count
+from django.db.models.functions import TruncDate
 from django_components import Component, register
+from occupancy.models import EntryEvent
 
 
 @register("busiest_days")
 class BusiestDays(Component):
     template_name = "busiest_days.html"
+
+    def get_context_data(self, items: int = 5) -> dict:
+        days_with_most_entries = (
+            EntryEvent.objects.annotate(date=TruncDate("timestamp"))
+            .values("date")
+            .annotate(entry_count=Count("id"))
+            .order_by("-entry_count")
+            .values("date", "entry_count")[:items]
+        )
+        labelled_days = LabelledDate.objects.filter(
+            date__in=(day["date"] for day in days_with_most_entries)
+        ).all()
+        day_labels = {day.date: day.label for day in labelled_days}
+
+        days = [
+            {
+                "date": day["date"],
+                "entry_count": day["entry_count"],
+                "label": day_labels.get(day["date"]),
+            }
+            for day in days_with_most_entries
+        ]
+        return {
+            "days": days,
+        }
--- innhabitserver/dashboard/models.py
+++ innhabitserver/dashboard/models.py
@@ -1,1 +1,9 @@
-# Create your models here.
+from django.db import models
+
+
+class LabelledDate(models.Model):
+    date = models.DateField(unique=True)
+    label = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.label} - {self.date}"
