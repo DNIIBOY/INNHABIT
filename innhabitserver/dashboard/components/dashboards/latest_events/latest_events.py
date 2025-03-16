2c6faba56703ce76e60020e2ac4177cdd051b072
DNIIBOY <daniel.nettelfield@gmail.com>
2025-03-16 21:43:59 +0100

    Somehow removed 100+ css clases without breaking anything

--- innhabitserver/dashboard/components/dashboards/latest_events/latest_events.py
+++ innhabitserver/dashboard/components/dashboards/latest_events/latest_events.py
@@ -1,6 +1,20 @@
+from django.db.models import BooleanField, Value
 from django_components import Component, register
+from occupancy.models import EntryEvent, ExitEvent
 
 
 @register("latest_events")
 class LatestEvents(Component):
     template_name = "latest_events.html"
+
+    def get_context_data(self) -> dict:
+        entry_events = EntryEvent.objects.annotate(
+            is_entry=Value(True, output_field=BooleanField())
+        ).prefetch_related("entrance")
+        exit_events = ExitEvent.objects.annotate(
+            is_entry=Value(False, output_field=BooleanField())
+        ).prefetch_related("entrance")
+        latest_events = entry_events.union(exit_events).order_by("-timestamp")[:4]
+        return {
+            "events": latest_events,
+        }
