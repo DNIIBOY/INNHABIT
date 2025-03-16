9b30407d9a6c8d2ff025e06b1b4e9439d970ca5a
DNIIBOY <daniel.nettelfield@gmail.com>
2025-03-16 22:38:12 +0100

    Add montly visitors

---
+++ innhabitserver/dashboard/components/dashboards/monthly_visitors/monthly_visitors.py
@@ -0,0 +1,48 @@
+from django.utils import timezone
+from django_components import Component, register
+from occupancy.models import EntryEvent
+
+
+@register("monthly_visitors")
+class MonthlyVisitors(Component):
+    template_name = "monthly_visitors.html"
+
+    def get_context_data(self) -> dict:
+        months = [
+            "Januar",
+            "Februar",
+            "Marts",
+            "April",
+            "Maj",
+            "Juni",
+            "Juli",
+            "August",
+            "September",
+            "Oktober",
+            "November",
+            "December",
+        ]
+        today = timezone.now().date()
+        entries = EntryEvent.objects.filter(
+            timestamp__date__year=today.year,
+            timestamp__date__month=today.month,
+        ).count()
+        last_month_entries = EntryEvent.objects.filter(
+            timestamp__date__year=today.year,
+            timestamp__date__month=today.month - 1,
+            timestamp__date__day__lte=today.day,
+        ).count()
+
+        if last_month_entries == 0:
+            last_month_entries = 1  # Avoid division by zero
+
+        return {
+            "montly_visitors": entries,
+            "last_month_visitors": last_month_entries,
+            "last_month_diff": entries - last_month_entries,
+            "last_month_percentage": round(
+                (entries - last_month_entries) / last_month_entries * 100, 2
+            ),
+            "date": today,
+            "month": months[today.month - 1],
+        }
