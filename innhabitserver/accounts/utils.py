acf0e3f5a3bc37655a6de013213e6f2f7c64d7d1
Daniel Nettelfield <daniel.nettelfield@gmail.com>
2025-03-17 16:07:20 +0100

    Start add user stuff

---
+++ innhabitserver/accounts/utils.py
@@ -0,0 +1,28 @@
+import requests
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
+from .tokens import account_activation_token
+
+User = get_user_model()
+
+
+def send_activation_email(user: User, host: str = "https://innhabit.dk") -> None:
+    b64uid = urlsafe_base64_encode(force_bytes(user.pk))
+    token = account_activation_token.make_token(user)
+    path = reverse("activate_account", args=[b64uid, token])
+
+    requests.post(
+        "https://api.eu.mailgun.net/v3/mg.sigmaboy.dk/messages",
+        auth=("api", settings.MAILGUN_API_KEY),
+        data={
+            "from": "noreply@sigmaboy.dk",
+            "to": [user.email],
+            "subject": "Innhabit - Konto aktivering",
+            "text": f"Tryk her for at aktivere din konto: {host}{path}",
+        },
+        timeout=5,
+    )
