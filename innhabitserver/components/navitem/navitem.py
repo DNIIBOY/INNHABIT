1627e33299452bfa824a5ae00e0812b9b4361599
Daniel Nettelfield <daniel.nettelfield@gmail.com>
2025-03-18 15:57:00 +0100

    Started new nav

--- innhabitserver/components/navitem/navitem.py
+++ innhabitserver/components/navitem/navitem.py
@@ -4,3 +4,6 @@ from django_components import Component, register
 @register("navitem")
 class Navitem(Component):
     template_name = "navitem.html"
+
+    def get_context_data(self, active: bool = False) -> dict:
+        return {"active": active}
