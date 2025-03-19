f715a10a10fbbe5229f1ee88ccf64f50dd40e0cf
Daniel Nettelfield <daniel.nettelfield@gmail.com>
2025-03-19 20:03:50 +0100

    Plot todays activity

--- innhabitserver/dashboard/components/dashboards/comparison_plot/comparison_plot.js
+++ innhabitserver/dashboard/components/dashboards/comparison_plot/comparison_plot.js
@@ -1,99 +1,107 @@
-var ctx2 = document.getElementById("chart-line").getContext("2d");
+function setupComparisonPlot(data) {
+    const comparisonChart = document.getElementById("comparison-chart").getContext("2d");
 
-var gradientStroke1 = ctx2.createLinearGradient(0, 230, 0, 50);
 
-gradientStroke1.addColorStop(1, "rgba(49, 169, 193, 0.2)");
-gradientStroke1.addColorStop(0.2, "rgba(72,72,176,0.0)");
+    const gradientStroke1 = comparisonChart.createLinearGradient(0, 230, 0, 50);
+    gradientStroke1.addColorStop(1, "rgba(49, 169, 193, 0.2)");
+    gradientStroke1.addColorStop(0.2, "rgba(72,72,176,0.0)");
 
-var gradientStroke2 = ctx2.createLinearGradient(0, 230, 0, 50);
+    const gradientStroke2 = comparisonChart.createLinearGradient(0, 230, 0, 50);
+    gradientStroke2.addColorStop(1, "rgba(33, 26, 81, 0.2)");
+    gradientStroke2.addColorStop(0.2, "rgba(72,72,176,0.0)");
 
-gradientStroke2.addColorStop(1, "rgba(33, 26, 81, 0.2)");
-gradientStroke2.addColorStop(0.2, "rgba(72,72,176,0.0)");
 
-new Chart(ctx2, {
-  type: "line",
-  data: {
-    labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "00"],
-    datasets: [
-      {
-        label: "Mobile apps",
-        tension: 0.4,
-        borderWidth: 0,
-        pointRadius: 0,
-        borderColor: "#31A9C1",
-        borderWidth: 3,
-        backgroundColor: gradientStroke1,
-        fill: true,
-        data: [50, 40, 300, 220, 500, 250, 400, 230, ],
-        maxBarThickness: 6,
-      },
-      {
-        label: "Websites",
-        tension: 0.4,
-        borderWidth: 0,
-        pointRadius: 0,
-        borderColor: "#201950",
-        borderWidth: 3,
-        backgroundColor: gradientStroke2,
-        fill: true,
-        data: [30, 90, 40, 140, 290, 290, 340, 230, 400, 200, 300, 200, 100, 300, 200, 100, 200, 100, 300, 200, 400, 200, 300, 100],
-        maxBarThickness: 6,
-      },
-    ],
-  },
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        display: false,
-      },
-    },
-    interaction: {
-      intersect: false,
-      mode: "index",
-    },
-    scales: {
-      y: {
-        grid: {
-          drawBorder: false,
-          display: true,
-          drawOnChartArea: true,
-          drawTicks: false,
-          borderDash: [5, 5],
+    new Chart(comparisonChart, {
+        type: "line",
+        data: {
+            labels: data["labels"],
+            datasets: [
+                {
+                    label: "Idag",
+                    tension: 0.4,
+                    borderWidth: 0,
+                    pointRadius: 0,
+                    borderColor: "#31A9C1",
+                    borderWidth: 3,
+                    backgroundColor: gradientStroke1,
+                    fill: true,
+                    data: data["today_counts"],
+                    maxBarThickness: 6,
+                },
+                {
+                    label: "Gennemsnitlig",
+                    tension: 0.4,
+                    borderWidth: 0,
+                    pointRadius: 0,
+                    borderColor: "#201950",
+                    borderWidth: 3,
+                    backgroundColor: gradientStroke2,
+                    fill: true,
+                    data: [],
+                    maxBarThickness: 6,
+                },
+            ],
         },
-        ticks: {
-          display: true,
-          padding: 10,
-          color: "#b2b9bf",
-          font: {
-            size: 11,
-            family: "Open Sans",
-            style: "normal",
-            lineHeight: 2,
-          },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: false,
+                },
+            },
+            interaction: {
+                intersect: false,
+                mode: "index",
+            },
+            scales: {
+                y: {
+                    grid: {
+                        drawBorder: false,
+                        display: true,
+                        drawOnChartArea: true,
+                        drawTicks: false,
+                        borderDash: [5, 5],
+                    },
+                    ticks: {
+                        display: true,
+                        padding: 10,
+                        color: "#b2b9bf",
+                        font: {
+                            size: 11,
+                            family: "Open Sans",
+                            style: "normal",
+                            lineHeight: 2,
+                        },
+                    },
+                },
+                x: {
+                    grid: {
+                        drawBorder: false,
+                        display: false,
+                        drawOnChartArea: false,
+                        drawTicks: false,
+                        borderDash: [5, 5],
+                    },
+                    ticks: {
+                        display: true,
+                        color: "#b2b9bf",
+                        padding: 20,
+                        font: {
+                            size: 11,
+                            family: "Open Sans",
+                            style: "normal",
+                            lineHeight: 2,
+                        },
+                    },
+                },
+            },
         },
-      },
-      x: {
-        grid: {
-          drawBorder: false,
-          display: false,
-          drawOnChartArea: false,
-          drawTicks: false,
-          borderDash: [5, 5],
-        },
-        ticks: {
-          display: true,
-          color: "#b2b9bf",
-          padding: 20,
-          font: {
-            size: 11,
-            family: "Open Sans",
-            style: "normal",
-            lineHeight: 2,
-          },
-        },
-      },
-    },
-  },
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const jsonElement = document.getElementById("comparison-json-element");
+    const data = JSON.parse(jsonElement.dataset.json);
+    setupComparisonPlot(data);
 });
