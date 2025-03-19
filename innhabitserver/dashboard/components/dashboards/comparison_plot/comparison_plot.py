8b55ec256c3a206344d9eaff411bfb74d2e8df47
Daniel Nettelfield <daniel.nettelfield@gmail.com>
2025-03-19 20:03:50 +0100

    Plot todays activity

--- innhabitserver/dashboard/components/dashboards/comparison_plot/comparison_plot.py
+++ innhabitserver/dashboard/components/dashboards/comparison_plot/comparison_plot.py
@@ -1,4 +1,36 @@
+import json
+
+import numpy as np
+from django.db.models import Count, ExpressionWrapper, IntegerField
+from django.db.models.functions import ExtractHour, ExtractMinute, Floor
+from django.db.models.query import QuerySet
+from django.utils import timezone
 from django_components import Component, register
+from occupancy.models import EntryEvent, ExitEvent
+
+
+def extract_count_per_interval(queryset: QuerySet, interval: int) -> np.array:
+    assert 60 % interval == 0, "Interval must be a divisor of 60"
+    intervals_per_hour = 60 // interval
+    groups = (
+        queryset.annotate(
+            hour=ExtractHour("timestamp"),
+            minute_group=ExpressionWrapper(
+                Floor((ExtractMinute("timestamp") / interval)),
+                output_field=IntegerField(),
+            ),
+        )
+        .values("hour", "minute_group")
+        .annotate(count=Count("id"))
+        .order_by("hour", "minute_group")
+    )
+
+    event_counts = np.zeros(24 * intervals_per_hour, dtype=int)
+    for item in groups:
+        event_counts[item["hour"] * intervals_per_hour + item["minute_group"]] = item[
+            "count"
+        ]
+    return event_counts
 
 
 @register("comparison_plot")
@@ -8,3 +40,32 @@ class ComparisonPlot(Component):
 
     class Media:
         js = ["https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.0.2/chart.min.js"]
+
+    def get_context_data(self, interval: int = 30) -> dict:
+        assert 60 % interval == 0, "Interval must be a divisor of 60"
+
+        today = timezone.now().date()
+        entries = extract_count_per_interval(
+            EntryEvent.objects.filter(timestamp__date=today), interval
+        )
+        exits = extract_count_per_interval(
+            ExitEvent.objects.filter(timestamp__date=today), interval
+        )
+        today_counts = entries.cumsum() - exits.cumsum()
+
+        labels = []
+        for hour in range(24):
+            for minute_group in range(0, 60, interval):
+                if minute_group == 0:
+                    labels.append(f"{hour:02d}")
+                else:
+                    labels.append("")  # Empty labels for minutes
+
+        context = {
+            "labels": labels,
+            "today_counts": today_counts.tolist(),
+        }
+        return {
+            **context,
+            "json_data": json.dumps(context),
+        }
--- innhabitserver/dashboard/components/dashboards/entrance_overview/entrance_overview.js
+++ innhabitserver/dashboard/components/dashboards/entrance_overview/entrance_overview.js
@@ -1,47 +1,52 @@
-const entriesChart = document.getElementById("entries-chart").getContext("2d");
-const exitsCharts = document.getElementById("exits-chart").getContext("2d");
+function setupEntranceCharts(data){
+    const entriesChart = document.getElementById("entries-chart").getContext("2d");
+    const exitsCharts = document.getElementById("exits-chart").getContext("2d");
 
-const jsonElement = document.getElementById("json-element");
-const data = JSON.parse(jsonElement.dataset.json);
-
-const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-        legend: {
-            display: false,
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: false,
+            },
         },
-    },
-    interaction: {
-        intersect: false,
-        mode: "index",
+        interaction: {
+            intersect: false,
+            mode: "index",
+        }
     }
+
+    new Chart(entriesChart, {
+        type: "doughnut",
+        data: {
+            labels: data["labels"],
+            datasets: [
+                {
+                    data: data["entries"],
+                    backgroundColor: data["colors"],
+                },
+            ],
+        },
+        options: options,
+    });
+
+    new Chart(exitsCharts, {
+        type: "doughnut",
+        data: {
+            labels: data["labels"],
+            datasets: [
+                {
+                    data: data["exits"],
+                    backgroundColor: data["colors"],
+                },
+            ],
+        },
+        options: options,
+    })
 }
 
-new Chart(entriesChart, {
-    type: "doughnut",
-    data: {
-        labels: data["labels"],
-        datasets: [
-            {
-                data: data["entries"],
-                backgroundColor: data["colors"],
-            },
-        ],
-    },
-    options: options,
-});
-
-new Chart(exitsCharts, {
-    type: "doughnut",
-    data: {
-        labels: data["labels"],
-        datasets: [
-            {
-                data: data["exits"],
-                backgroundColor: data["colors"],
-            },
-        ],
-    },
-    options: options,
+document.addEventListener("DOMContentLoaded", function() {
+    const jsonElement = document.getElementById("entrance-json-element");
+    const data = JSON.parse(jsonElement.dataset.json);
+    setupEntranceCharts(data);
 });
