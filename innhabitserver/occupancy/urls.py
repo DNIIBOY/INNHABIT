1dd70bc49c205e681a81a5eb5a773e9221725092
DNIIBOY <daniel.nettelfield@gmail.com>
2025-03-23 00:36:56 +0100

    Add basic system page

--- innhabitserver/occupancy/urls.py
+++ innhabitserver/occupancy/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("configuration/", views.configuration, name="configuration"),
 ]
--- innhabitserver/occupancy/views.py
+++ innhabitserver/occupancy/views.py
@@ -1,6 +1,16 @@
+from django.db.models import Max
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from occupancy.models import Entrance
 
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "index.html")
+
+
+def configuration(request: HttpRequest) -> HttpResponse:
+    entrances = Entrance.objects.annotate(
+        latest_entry=Max("entries__timestamp"),
+        latest_exit=Max("exits__timestamp"),
+    ).all()
+    return render(request, "configuration.html", {"entrances": entrances})
