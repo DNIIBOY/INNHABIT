b7b98e33e4c3d8efe0dbdc2a9f19387da6c9f890
Daniel Nettelfield <daniel.nettelfield@gmail.com>
2025-03-25 13:11:14 +0100

    Start config thing

--- innhabitserver/occupancy/views.py
+++ innhabitserver/occupancy/views.py
@@ -1,6 +1,7 @@
-from django.db.models import Max
+from django.db.models import F, Max
+from django.db.models.functions import Greatest
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from occupancy.models import Entrance
 
 
@@ -9,8 +10,22 @@ def index(request: HttpRequest) -> HttpResponse:
 
 
 def configuration(request: HttpRequest) -> HttpResponse:
-    entrances = Entrance.objects.annotate(
-        latest_entry=Max("entries__timestamp"),
-        latest_exit=Max("exits__timestamp"),
-    ).all()
+    entrances = (
+        Entrance.objects.annotate(
+            latest_entry=Max("entries__timestamp"),
+            latest_exit=Max("exits__timestamp"),
+            latest_event=Greatest(F("latest_entry"), F("latest_exit")),
+        )
+        .order_by("id")
+        .all()
+    )
     return render(request, "configuration.html", {"entrances": entrances})
+
+
+def configure_entrance(request: HttpRequest, pk: int) -> HttpRequest:
+    entrance = get_object_or_404(Entrance, pk=pk)
+    context = {
+        "entrance": entrance,
+        "device": getattr(entrance, "device", None),
+    }
+    return render(request, "configure_entrance.html", context)
