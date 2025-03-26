2c52f8dcdc0ddb23c31ff93cc1b4e8088658323f
DNIIBOY <daniel.nettelfield@gmail.com>
2025-03-26 21:29:55 +0100

    Small fixes

--- innhabitserver/occupancy/admin.py
+++ innhabitserver/occupancy/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from . import models
 
+admin.site.register(models.Device)
 admin.site.register(models.Entrance)
 admin.site.register(models.EntryEvent)
 admin.site.register(models.ExitEvent)
