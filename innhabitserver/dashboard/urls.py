82ba37000b985ad706db2b60379fa56149cdd2df
Daniel Nettelfield <daniel.nettelfield@gmail.com>
2025-03-26 11:49:54 +0100

    Make latest_events auto update

--- innhabitserver/dashboard/urls.py
+++ innhabitserver/dashboard/urls.py
@@ -1,3 +1,4 @@
+from dashboard.components.dashboards.latest_events.latest_events import LatestEvents
 from django.urls import path
 
 from . import views
@@ -5,4 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="dashboard"),
     path("insights/", views.insights, name="insights"),
+    path("components/latest_events/", LatestEvents.as_view(), name="latest_events"),
 ]
