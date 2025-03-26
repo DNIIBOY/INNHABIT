391cdddf732e164ac31e3b1d0041ef8996b3f6e3
gjellerhjort <mads.gjellerod.privat@gmail.com>
2025-03-26 10:09:22 +0100

    hello rtsp server

--- innhabit-device/include/rtspStreamManager.h
+++ innhabit-device/include/rtspStreamManager.h
@@ -1,23 +1,28 @@
 #ifndef RTSP_STREAM_MANAGER_H
 #define RTSP_STREAM_MANAGER_H
 
+#include <opencv2/opencv.hpp>
 #include <queue>
+#include <thread>
 #include <mutex>
 #include <condition_variable>
 #include <atomic>
-#include <thread>
-#include <opencv2/opencv.hpp>
+#include <chrono>
+#include <string>  // Added for std::string
 
 class RTSPStreamManager {
 public:
-    RTSPStreamManager(std::queue<cv::Mat>& displayQueue,
-                     std::mutex& displayMutex,
-                     std::condition_variable& displayCV,
-                     std::atomic<bool>& shouldExit);
+    RTSPStreamManager(std::queue<cv::Mat>& displayQueue, 
+                      std::mutex& displayMutex,
+                      std::condition_variable& displayCV,
+                      std::atomic<bool>& shouldExit,
+                      const std::string& rtspPath = "/stream",
+                      const std::string& host = "127.0.0.1",
+                      int port = 8554);
     
     void start();
     void join();
-
+    
 private:
     void displayFrames();
     
@@ -26,6 +31,9 @@ private:
     std::condition_variable& m_displayCV;
     std::atomic<bool>& m_shouldExit;
     std::thread m_thread;
+    std::string m_rtspPath;  // e.g., "/stream"
+    std::string m_host;      // e.g., "127.0.0.1"
+    int m_port;              // e.g., 8554
 };
 
-#endif
+#endif // RTSP_STREAM_MANAGER_H
