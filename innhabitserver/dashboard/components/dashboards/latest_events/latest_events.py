c2aff110a3ddc916403c6c80adae5ff20a954ad3
Daniel Nettelfield <daniel.nettelfield@gmail.com>
2025-03-27 23:51:15 +0100

    Add infinite_scroll to export page

--- innhabitserver/dashboard/components/dashboards/latest_events/latest_events.py
+++ innhabitserver/dashboard/components/dashboards/latest_events/latest_events.py
@@ -1,6 +1,9 @@
+import urllib.parse
+
+from dashboard.utils import FakeMetadata
 from django.db.models import BooleanField, Value
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django_components import Component, register
 from occupancy.models import Entrance, EntryEvent, ExitEvent
 
@@ -10,17 +13,31 @@ class LatestEvents(Component):
     template_name = "latest_events.html"
 
     def get(self, request: HttpRequest) -> HttpResponse:
-        items = int(request.GET.get("items", 4))
         entrance = request.GET.get("entrance")
         entrance = int(entrance) if entrance else None
-        kwargs = {"items": items, "entrance": entrance}
+        kwargs = {
+            "items": int(request.GET.get("items", 4)),
+            "offset": int(request.GET.get("offset", 0)),
+            "entrance": entrance,
+            "infinite_scroll": request.GET.get("infinite_scroll", "False").lower()
+            == "true",
+            "hide_title": request.GET.get("hide_title", "False").lower() == "true",
+            "timestamp_format": request.GET.get("timestamp_format", "H:i"),
+        }
 
-        return self.render_to_response(request=request, kwargs=kwargs)
+        if not request.htmx:
+            return self.render_to_response(request=request, kwargs=kwargs)
+
+        with self._with_metadata(FakeMetadata(request)):
+            context = self.get_context_data(**kwargs)
+        return render(request, self.template_name + "#event_rows", context)
 
     def get_context_data(
         self,
         items: int = 4,
+        offset: int = 0,
         entrance: Entrance | int | None = None,
+        infinite_scroll: bool = False,
         hide_title: bool = False,
         timestamp_format: str = "H:i",
     ) -> dict:
@@ -29,13 +46,13 @@ class LatestEvents(Component):
         entry_events = EntryEvent.objects.annotate(
             is_entry=Value(True, output_field=BooleanField())
         ).prefetch_related("entrance")
-        if entrance:
-            entry_events = entry_events.filter(entrance=entrance)
 
         exit_events = ExitEvent.objects.annotate(
             is_entry=Value(False, output_field=BooleanField())
         ).prefetch_related("entrance")
+
         if entrance:
+            entry_events = entry_events.filter(entrance=entrance)
             exit_events = exit_events.filter(entrance=entrance)
 
         view_entry = self.request.user.has_perm("occupancy.view_entry_event")
@@ -50,10 +67,22 @@ class LatestEvents(Component):
         else:
             events = EntryEvent.objects.none()
 
-        latest_events = events.order_by("-timestamp")[:items]
+        latest_events = events.order_by("-timestamp")[offset : offset + items]
 
-        return {
-            "events": latest_events,
+        kwargs = {
+            "items": items,
+            "offset": offset + items,
+            "entrance": entrance.pk if entrance else "",  # type: ignore[union-attr]
+            "infinite_scroll": infinite_scroll,
             "hide_title": hide_title,
             "timestamp_format": timestamp_format,
+        }
+        scroll_params = urllib.parse.urlencode(kwargs)
+        kwargs["offset"] = offset
+
+        return {
+            "params": urllib.parse.urlencode(kwargs),
+            "scroll_params": scroll_params,
+            "events": latest_events,
+            **kwargs,
         }
